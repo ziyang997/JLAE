0d1916466e15f3c1b292df726985e39f61866c85
ziyang997 <56155063+ziyang997@users.noreply.github.com>
2020-12-21 21:44:45 +0800

    Update evaluate.py

--- evaluate.py
+++ evaluate.py
@@ -29,21 +29,23 @@ def NDCG_at_k(predicted_list, ground_truth, k):
     return dcg / idcg
 
 
-# calculate precision@k, recall@k, NDCG@k, where k = 1,5,10,15
+# calculate precision@k, recall@k, NDCG@k, where k = 1,5,10,15,20
 def user_precision_recall_ndcg(new_user_prediction, test):
     dcg_list = []
 
     # compute the number of true positive items at top k
-    count_1, count_5, count_10, count_15 = 0, 0, 0, 0
-    for i in range(15):
+    count_1, count_5, count_10, count_15, count_20 = 0, 0, 0, 0, 0
+    for i in range(20):
         if i == 0 and new_user_prediction[i][0] in test:
             count_1 = 1.0
         if i < 5 and new_user_prediction[i][0] in test:
             count_5 += 1.0
         if i < 10 and new_user_prediction[i][0] in test:
             count_10 += 1.0
-        if new_user_prediction[i][0] in test:
+        if i < 15 and new_user_prediction[i][0] in test:
             count_15 += 1.0
+        if new_user_prediction[i][0] in test:
+            count_20 += 1.0
             dcg_list.append(1)
         else:
             dcg_list.append(0)
@@ -54,13 +56,13 @@ def user_precision_recall_ndcg(new_user_prediction, test):
     ndcg_tmp_5 = NDCG_at_k(dcg_list, idcg_list, 5)
     ndcg_tmp_10 = NDCG_at_k(dcg_list, idcg_list, 10)
     ndcg_tmp_15 = NDCG_at_k(dcg_list, idcg_list, 15)
-
+    ndcg_tmp_20= NDCG_at_k(dcg_list, idcg_list, 20)
     # precision@k
     precision_1 = count_1
     precision_5 = count_5 / 5.0
     precision_10 = count_10 / 10.0
     precision_15 = count_15 / 15.0
-
+    precision_20 = count_20 / 20.0
     l = len(test)
     if l == 0:
         l = 1
@@ -69,22 +71,22 @@ def user_precision_recall_ndcg(new_user_prediction, test):
     recall_5 = count_5 / l
     recall_10 = count_10 / l
     recall_15 = count_15 / l
-
+    recall_20 = count_20 / l
     # return precision, recall, ndcg_tmp
-    return np.array([precision_1, precision_5, precision_10, precision_15]), \
-           np.array([recall_1, recall_5, recall_10, recall_15]), \
-           np.array([ndcg_tmp_1, ndcg_tmp_5, ndcg_tmp_10, ndcg_tmp_15])
+    return np.array([precision_1, precision_5, precision_10, precision_15, precision_20]), \
+           np.array([recall_1, recall_5, recall_10, recall_15, recall_20]), \
+           np.array([ndcg_tmp_1, ndcg_tmp_5, ndcg_tmp_10, ndcg_tmp_15, ndcg_tmp_20])
 
 
 # calculate the metrics of the result
 def test_model_all(prediction, test_mask, train_mask):
-    precision_1, precision_5, precision_10, precision_15 = 0.0000, 0.0000, 0.0000, 0.0000
-    recall_1, recall_5, recall_10, recall_15 = 0.0000, 0.0000, 0.0000, 0.0000
-    ndcg_1, ndcg_5, ndcg_10, ndcg_15 = 0.0000, 0.0000, 0.0000, 0.0000
-    precision = np.array([precision_1, precision_5, precision_10, precision_15])
-    recall = np.array([recall_1, recall_5, recall_10, recall_15])
-    ndcg = np.array([ndcg_1, ndcg_5, ndcg_10, ndcg_15])
 
+    precision_1, precision_5, precision_10, precision_15, precision_20 = 0.0000, 0.0000, 0.0000, 0.0000, 0.0000
+    recall_1, recall_5, recall_10, recall_15, recall_20 = 0.0000, 0.0000, 0.0000, 0.0000, 0.0000
+    ndcg_1, ndcg_5, ndcg_10, ndcg_15, ndcg_20 = 0.0000, 0.0000, 0.0000, 0.0000, 0.0000
+    precision = np.array([precision_1, precision_5, precision_10, precision_15, precision_20])
+    recall = np.array([recall_1, recall_5, recall_10, recall_15, recall_20])
+    ndcg = np.array([ndcg_1, ndcg_5, ndcg_10, ndcg_15, ndcg_20])
     prediction = prediction + train_mask * -1000000.0
 
     user_num = prediction.shape[0]
@@ -93,13 +95,13 @@ def test_model_all(prediction, test_mask, train_mask):
         u_test = np.where(u_test == 1)[0]  # the indices of the true positive items in the test set
         u_pred = prediction[u, :]
 
-        top15_item_idx_no_train = np.argpartition(u_pred, -15)[-15:]
-        top15 = (np.array([top15_item_idx_no_train, u_pred[top15_item_idx_no_train]])).T
-        top15 = sorted(top15, key=itemgetter(1), reverse=True)
+        top20_item_idx_no_train = np.argpartition(u_pred, -20)[-20:]
+        top20 = (np.array([top20_item_idx_no_train, u_pred[top20_item_idx_no_train]])).T
+        top20 = sorted(top20, key=itemgetter(1), reverse=True)
 
         # calculate the metrics
         if not len(u_test) == 0:
-            precision_u, recall_u, ndcg_u = user_precision_recall_ndcg(top15, u_test)
+            precision_u, recall_u, ndcg_u = user_precision_recall_ndcg(top20, u_test)
             precision += precision_u
             recall += recall_u
             ndcg += ndcg_u
@@ -110,14 +112,15 @@ def test_model_all(prediction, test_mask, train_mask):
     precision /= user_num
     recall /= user_num
     ndcg /= user_num
-    print('precision_1\t[%.7f],\t||\t precision_5\t[%.7f],\t||\t precision_10\t[%.7f],\t||\t precision_15\t[%.7f]' \
+    print('precision_1\t[%.7f],\t||\t precision_5\t[%.7f],\t||\t precision_10\t[%.7f],\t||\t precision_15\t[%.7f],\t||\t precision_20\t[%.7f]' \
           % (precision[0],
              precision[1],
              precision[2],
-             precision[3]))
-    print('recall_1   \t[%.7f],\t||\t recall_5   \t[%.7f],\t||\t recall_10   \t[%.7f],\t||\t recall_15   \t[%.7f]' \
+             precision[3],
+             precision[4]))
+    print('recall_1 \t[%.7f],\t||\t recall_5   \t[%.7f],\t||\t recall_10   \t[%.7f],\t||\t recall_15   \t[%.7f], \t||\t recall_20  \t[%.7f]' \
           % (recall[0], recall[1],
-             recall[2], recall[3]))
+             recall[2], recall[3], recall[4]))
     f_measure_1 = 2 * (precision[0] * recall[0]) / (precision[0] + recall[0]) if not precision[0] + recall[
         0] == 0 else 0
     f_measure_5 = 2 * (precision[1] * recall[1]) / (precision[1] + recall[1]) if not precision[1] + recall[
@@ -126,18 +129,24 @@ def test_model_all(prediction, test_mask, train_mask):
         2] == 0 else 0
     f_measure_15 = 2 * (precision[3] * recall[3]) / (precision[3] + recall[3]) if not precision[3] + recall[
         3] == 0 else 0
-    print('f_measure_1\t[%.7f],\t||\t f_measure_5\t[%.7f],\t||\t f_measure_10\t[%.7f],\t||\t f_measure_15\t[%.7f]' \
+    f_measure_20 = 2 * (precision[4] * recall[4]) / (precision[4] + recall[4]) if not precision[4] + recall[
+        4] == 0 else 0
+    print('f_measure_1\t[%.7f],\t||\t f_measure_5\t[%.7f],\t||\t f_measure_10\t[%.7f],\t||\t f_measure_15\t[%.7f], \t||\t f_measure_20\t[%.7f]' \
           % (f_measure_1,
              f_measure_5,
              f_measure_10,
-             f_measure_15))
-    f_score = [f_measure_1, f_measure_5, f_measure_10, f_measure_15]
-    print('ndcg_1     \t[%.7f],\t||\t ndcg_5     \t[%.7f],\t||\t ndcg_10     \t[%.7f],\t||\t ndcg_15     \t[%.7f]' \
+             f_measure_15,
+             f_measure_20))
+    f_score = [f_measure_1, f_measure_5, f_measure_10, f_measure_15, f_measure_20]
+    print('ndcg_1     \t[%.7f],\t||\t ndcg_5     \t[%.7f],\t||\t ndcg_10     \t[%.7f],\t||\t ndcg_15     \t[%.7f],\t||\t ndcg_20     \t[%.7f]' \
           % (ndcg[0],
              ndcg[1],
              ndcg[2],
-             ndcg[3]))
+             ndcg[3],
+             ndcg[4]))
     return precision, recall, f_score, ndcg
+
+
 
 
 def metric_record(precision, recall, f_score, NDCG, args, metric_path):  # record all the results' details into files
@@ -289,7 +298,6 @@ def test_model_factor(prediction, test_mask, train_mask):
 
 
 def test_all(sess, model):
-    Decoder = sess.run(model.rating, {model.users: list(range(model.num_users)),model.input_R_U: model.train_R})
+    Decoder = sess.run(model.decoder, {model.users: list(range(model.num_users)),model.input_R_U: model.train_R, model.drop:1})
     Decoder = np.array(Decoder)
-    Decoder = np.delete(Decoder, -1, axis=1)
     test_model_all(Decoder, model.test_R, model.train_R)
